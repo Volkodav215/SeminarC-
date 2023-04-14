1d8841e946900bd49f320df35d3a7694cd4b6f5d
Vladislav <v.kamenshikov@mail.ru>
2023-04-14 22:48:43 +0300

    HomeWork2 created

--- Program.cs
+++ Program.cs
@@ -1,116 +1,123 @@
-﻿// Урок 1. Знакомство с языком программирования С#
+﻿// // Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
-// // Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// // 456 -> 5
+// // 782 -> 8
+// // 918 -> 1
 
-// // a = 5; b = 7 -> max = 7
-// // a = 2 b = 10 -> max = 10
-// // a = -9 b = -3 -> max = -3
-// Console.WriteLine("Введите первое число: ");
-// int a = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите второе число: ");
-// int b = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите трехзначное число");
+// int num = Convert.ToInt32(Console.ReadLine());
 
-// if(a>b)
+// if(num > 99 && num < 1000)
 // {
-//     Console.Write("max = ");
-//     Console.WriteLine(a);
-//     Console.Write("min = ");
-//     Console.WriteLine(b);
+//     num = (num/10)%10;
+//     Console.WriteLine(num);
 // }
 // else
 // {
-//     Console.Write("max = ");
-//     Console.WriteLine(b);
-//     Console.Write("min = ");
-//     Console.WriteLine(a);
+//     Console.WriteLine("Вы ввели не трехзначное число!!!");
 // }
 
-///********************************************************************************///
+///******************************************************************************************************************///
 
-// // Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
-// // 2, 3, 7 -> 7
-// // 44 5 78 -> 78
-// // 22 3 9 -> 22
+// // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// // 645 -> 5
+// // 78 -> третьей цифры нет
+// // 32679 -> 6
 
-// Console.WriteLine("Введите первое число: ");
-// int a = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите второе число: ");
-// int b = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите третье число: ");
-// int c = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
 
-// //int[] arr = {a,b,c};
-// int max = a;
-
-// if (max > b)
+// if(num > 99)
 // {
-//     if (max > c)
+//     while(num>999)
 //     {
-//         Console.Write("max = ");
-//         Console.WriteLine(a);
+//        num/=10; 
+//     }
+
+//     Console.WriteLine(num%10);
+// }
+// else
+// {
+//     Console.WriteLine("В вашем числе третьей цифры нет");
+// }
+
+///************************************************************************************************************************///
+
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+//1й способ!!!
+
+// Console.WriteLine("Введите номер дня недели - (от 1 до 7)");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+
+
+// if(num>0 && num<8 )
+// {
+//     if(num == 6 || num == 7)
+//     {
+//         Console.WriteLine("выходной");
 //     }
 //     else
 //     {
-//         Console.Write("max = ");
-//         Console.WriteLine(c);
+//         Console.WriteLine("будний день");
 //     }
 // }
 // else
 // {
-//     max = b; // так как b > a - максимальное значение теперь у b
+//     Console.WriteLine("Вы ввели число не соответствующее дню недели!");
+// }
 
-//     if (max > c)
+//2й способ!!!
+
+// Console.WriteLine("Введите номер дня недели - (от 1 до 7)");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// switch(num)
+// {
+//     case 1:
 //     {
-//         Console.Write("max = ");
-//         Console.WriteLine(b);
+//         Console.WriteLine("Пн - будний день");
+//         break;
 //     }
-//     else
+//     case 2:
 //     {
-//         Console.Write("max = ");
-//         Console.WriteLine(c);
+//         Console.WriteLine("Вт - будний день");
+//         break;
 //     }
-// }
-
-///********************************************************************************///
-
-
-// // Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
-
-// // 4 -> да
-// // -3 -> нет
-// // 7 -> нет
-// Console.WriteLine("Введите число: ");
-// int a = Convert.ToInt32(Console.ReadLine());
-
-// if(a % 2 == 0)
-// {
-//     Console.Write(a);
-//     Console.WriteLine(" - Вы ввели четное число");
-// }
-// else
-// {
-//     Console.Write(a);
-//     Console.WriteLine(" - Вы ввели не четное число");
-// }
-
-///********************************************************************************///
-
-// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
-
-// 5 -> 2, 4
-// 8 -> 2, 4, 6, 8
-
-// Console.Write("Введите число: ");
-// int N = Convert.ToInt32(Console.ReadLine());
-
-// for(int i = 1; i <= N; i++ )
-// {
-//     if(i % 2 == 0 )
+//     case 3:
 //     {
-//         Console.WriteLine(i);
+//         Console.WriteLine("Ср - будний день");
+//         break;
+//     }
+//     case 4:
+//     {
+//         Console.WriteLine("Чт - будний день");
+//         break;
+//     }
+//     case 5:
+//     {
+//         Console.WriteLine("Пт - будний день");
+//         break;
+//     }
+//     case 6:
+//     {
+//         Console.WriteLine("Сб - выходной");
+//         break;
+//     }
+//     case 7:
+//     {
+//         Console.WriteLine("Вскр - выходной");
+//         break;
+//     }
+//     default:
+//     {
+//         Console.WriteLine("Вы ввели число не соответствующее дню недели!");
+//         break;
 //     }
 // }
-   
-
-   
