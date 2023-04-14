47c7b301a10d40750e2359180dca14c0779471ab
Vladislav <v.kamenshikov@mail.ru>
2023-04-14 21:53:43 +0300

    Home Work created

--- Program.cs
+++ Program.cs
@@ -1,2 +1,116 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Урок 1. Знакомство с языком программирования С#
+
+// // Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// // a = 5; b = 7 -> max = 7
+// // a = 2 b = 10 -> max = 10
+// // a = -9 b = -3 -> max = -3
+// Console.WriteLine("Введите первое число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// if(a>b)
+// {
+//     Console.Write("max = ");
+//     Console.WriteLine(a);
+//     Console.Write("min = ");
+//     Console.WriteLine(b);
+// }
+// else
+// {
+//     Console.Write("max = ");
+//     Console.WriteLine(b);
+//     Console.Write("min = ");
+//     Console.WriteLine(a);
+// }
+
+///********************************************************************************///
+
+// // Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// // 2, 3, 7 -> 7
+// // 44 5 78 -> 78
+// // 22 3 9 -> 22
+
+// Console.WriteLine("Введите первое число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите третье число: ");
+// int c = Convert.ToInt32(Console.ReadLine());
+
+// //int[] arr = {a,b,c};
+// int max = a;
+
+// if (max > b)
+// {
+//     if (max > c)
+//     {
+//         Console.Write("max = ");
+//         Console.WriteLine(a);
+//     }
+//     else
+//     {
+//         Console.Write("max = ");
+//         Console.WriteLine(c);
+//     }
+// }
+// else
+// {
+//     max = b; // так как b > a - максимальное значение теперь у b
+
+//     if (max > c)
+//     {
+//         Console.Write("max = ");
+//         Console.WriteLine(b);
+//     }
+//     else
+//     {
+//         Console.Write("max = ");
+//         Console.WriteLine(c);
+//     }
+// }
+
+///********************************************************************************///
+
+
+// // Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// // 4 -> да
+// // -3 -> нет
+// // 7 -> нет
+// Console.WriteLine("Введите число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// if(a % 2 == 0)
+// {
+//     Console.Write(a);
+//     Console.WriteLine(" - Вы ввели четное число");
+// }
+// else
+// {
+//     Console.Write(a);
+//     Console.WriteLine(" - Вы ввели не четное число");
+// }
+
+///********************************************************************************///
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+// Console.Write("Введите число: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+
+// for(int i = 1; i <= N; i++ )
+// {
+//     if(i % 2 == 0 )
+//     {
+//         Console.WriteLine(i);
+//     }
+// }
+   
+
+   
